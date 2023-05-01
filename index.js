a60ef37bd36f5229e72818deae29f88788d13da8
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-05-01 12:05:50 -0700

    adding CR functionality

--- index.js
+++ index.js
@@ -10,6 +10,50 @@ app.get("/",(req,res)=>{
     res.send("this is the homepage!")
 })
 
+app.get("/api/animals",(req,res)=>{
+    Animal.findAll().then(animals=>{
+        res.json(animals)
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:"error occurred",err})
+    })
+})
+app.get("/api/animals/searchname/:name",(req,res)=>{
+    Animal.findAll({
+        where:{
+            name:req.params.name
+        }
+    }).then(animals=>{
+        res.json(animals)
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:"error occurred",err})
+    })
+})
+
+app.get("/api/animals/:id",(req,res)=>{
+    Animal.findByPk(req.params.id).then(animal=>{
+        res.json(animal)
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:"error occurred",err})
+    })
+})
+
+app.post("/api/animals",(req,res)=>{
+    Animal.create({
+        name:"Racoony",
+        age:1,
+        species:"Raccoon",
+        notes:"raccoons are very bold."
+    }).then(newAni=>{
+        res.json(newAni)
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:"error occurred",err})
+    })
+})
+
 sequelize.sync({force:false}).then(()=>{
     app.listen(PORT,()=>{
         console.log(`listenin to port ${PORT}!`)
