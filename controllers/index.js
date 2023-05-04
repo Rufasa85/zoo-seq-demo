3d7e9c83b1d6707ced57913b6d3e12b129fd90a4
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-05-04 13:05:51 -0700

    adding session data

--- controllers/index.js
+++ controllers/index.js
@@ -8,6 +8,15 @@ router.get("/",(req,res)=>{
     res.send("this is the homepage!")
 })
 
+router.get("/sessiondata",(req,res)=>{
+    res.json(req.session);
+})
+
+router.get("/sessioncolor/:color",(req,res)=>{
+    req.session.favColor=req.params.color;
+    res.json(req.session);
+})
+
 router.use("/api/animals",animalRoutes)
 router.use("/api/zoos",zooRoutes)
 
--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const sequelize = require("./config/connection")
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT ||3000;
 
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge:1000*60*60*2
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session(sess))
 
 const allRoutes = require("./controllers")
 
@@ -16,3 +32,6 @@ sequelize.sync({force:false}).then(()=>{
         console.log(`listenin to port ${PORT}!`)
     })
 })
+
+
+
